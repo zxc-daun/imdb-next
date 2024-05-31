6afa7d2c6306f705b413bdfdf423bf67819cb4cf
zxc-daun <shokhzod.rakhmatov@narxoz.kz>
2024-05-31 19:00:42 +0500

    problems with globals.css

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,28 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header';
+import Providers from './Providers';
+import Navbar from '@/components/Navbar';
+import SearchBox from '@/components/SearchBox';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: "IMDb clone",
-  description: "This is a movie database clone",
+  title: 'IMDb clone',
+  description: 'This is a movie database clone',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <Providers>
+          <Header />
+          <Navbar />
+          <SearchBox />
+          {children}
+        </Providers>
+      </body>
     </html>
   );
 }
